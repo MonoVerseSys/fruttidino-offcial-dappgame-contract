7e9f72d6ffb10398e9342c5a5e51a432adb4b9f4
hodong <hodong.kim@monoverse.io>
2022-09-07 14:46:15 +0900

    bet

--- scripts/DinoArcade/betToken.ts
+++ scripts/DinoArcade/betToken.ts
@@ -4,18 +4,15 @@ import { Config } from '../utils'
 import configJson from './_config.json'
 const config: Config = configJson
 async function main() {
-    const amount = ethers.utils.parseEther('0.01')
-
-    const c = await utils.attach({
-        contractName: 'DinoArcade',
-        deployedAddress: config.networks[utils.getNetwork()],
-    })
-    const receipt = await c.betCoin({ value: ethers.utils.parseEther('0.01') })
-    console.log(receipt)
-
-    const tx = await receipt.wait()
-
-    console.log(tx)
+    // const amount = ethers.utils.parseEther('0.01')
+    // const c = await utils.attach({
+    //     contractName: 'DinoArcade',
+    //     deployedAddress: config.networks[utils.getNetwork()],
+    // })
+    // const receipt = await c.betCoin({ value: ethers.utils.parseEther('0.01') })
+    // console.log(receipt)
+    // const tx = await receipt.wait()
+    // console.log(tx)
 }
 
 // We recommend this pattern to be able to use async/await everywhere
