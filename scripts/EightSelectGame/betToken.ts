cb8e6b70d67adee5b8a41fe24853b0c5c4c05eb0
hodong <hodong.kim@monoverse.io>
2022-09-15 13:36:42 +0900

    script 정리

--- scripts/EightSelectGame/betToken.ts
+++ scripts/EightSelectGame/betToken.ts
@@ -7,10 +7,6 @@ async function main() {
     // const amount = ethers.utils.parseEther('0.01')
 
     const gameContractAddress = config.networks[utils.getNetwork()]
-    // const c = await utils.attach({
-    //     contractName: 'c',
-    //     deployedAddress: gameContractAddress,
-    // })
 
     const network = utils.getNetwork()
     let erc20Address = ''
@@ -22,44 +18,15 @@ async function main() {
 
     const signers = await utils.singers()
     const fdt = await ethers.getContractAt('FdtToken', erc20Address, signers[0])
-    // console.log(fdt)
     let betAmount = ethers.utils.parseEther('1')
     const data = ethers.utils.defaultAbiCoder.encode(['uint256[]'], [[1, 2, 3, 4, 5, 6, 7]])
-    // const data2 = ethers.utils.solidityPack(['uint256[]'], [[1, 2, 3]])
     console.log(data)
-    // console.log(fdt['transferAndCall(address,uint256,bytes)'])
     const receipt = await fdt['transferAndCall(address,uint256,bytes)'](gameContractAddress, betAmount, data)
     console.log(receipt)
     const result = await receipt.wait()
     console.log(result)
-
-    // let betAmount = ethers.utils.parseEther('1')
-
-    // const allowance = await fdt.allowance(signers[0].address, gameContractAddress)
-
-    // console.log(`betAmount: ${ethers.utils.formatEther(betAmount)}`)
-    // console.log(`allowance: ${ethers.utils.formatEther(allowance)}`)
-
-    // const balance = await fdt.balanceOf(signers[0].address)
-    // console.log(`user balance : ${signers[0].address}, balance: ${ethers.utils.formatEther(balance)}`)
-
-    // if (allowance.lt(betAmount)) {
-    //     console.error('토큰 허용금액 부족')
-    //     console.error('allowance start~!')
-    //     const receipt = await fdt.approve(gameContractAddress, betAmount)
-    //     console.log('allowance receipt:', receipt)
-    //     const tx = await receipt.wait()
-    //     console.log('allowance tx:', tx)
-    // }
-    // // 1 ~ 2
-    // const receipt = await c.betFdt(betAmount, [1, 2, 3, 4, 5, 6, 7])
-    // console.log('bet receipt: ', receipt)
-    // const tx = await receipt.wait()
-    // console.log('bet tx:', tx)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
     console.error(error)
     process.exitCode = 1
